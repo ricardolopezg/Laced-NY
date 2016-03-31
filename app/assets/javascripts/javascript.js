f20f33ca87ee678bb1e1754c244e7c0527ca9a8c
Jesse <jessehwang86@yahoo.com>
2016-03-31 17:26:29 -0400

    landing page styled

---
+++ app/assets/javascripts/javascript.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+  console.log("Yo")
+  $("#intro").fadeOut(20)
+  // $("#intro").delay(4000).fadeOut(2000)
+
+})
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def index
+    @products = Product.find(348).image2
+  end
 end
