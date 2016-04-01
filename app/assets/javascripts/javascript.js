f564f04bce5a4cb3004ee46850ac1b59d01f31c9
Jesse <jessehwang86@yahoo.com>
2016-04-01 16:34:41 -0400

    jkdfls;a

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -50,16 +50,54 @@ $(document).ready(function() {
 
 
 // MASONRY
-  var $grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
+// external js: isotope.pkgd.js
+
+  // init Isotope
+  var $container = $('.isotope').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    getSortData: {
+      name: '.name',
+      symbol: '.symbol',
+      number: '.number parseInt',
+      category: '[data-category]',
+      weight: function( itemElem ) {
+        var weight = $( itemElem ).find('.weight').text();
+        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      }
+    }
   });
 
-  $grid.on( 'click', '.grid-item', function() {
-    // change size of item via class
-    $( this ).toggleClass('grid-item--gigante');
-    // trigger layout
-    $grid.masonry();
-    $(this).children("span").toggleClass("desc");
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $container.isotope({ filter: filterValue });
+  });
+
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
   });
 
 }); // end doc ready
