84c690c6db6da56223399cf574cc37b0aa0017c9
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-04-04 17:11:37 -0400

    pushing for heroku sendgrid test

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,35 +1,35 @@
 class ContactController < ApplicationController
 
-  def index
+  # def index
     
-  end
+  # end
 
-  def create
-    client = SendGrid::Client.new(api_key: ENV["SENDGRID_LACED_EMAIL_API"])
+  # def create
+  #   client = SendGrid::Client.new(api_key: ENV["SENDGRID_LACED_EMAIL_API"])
 
-    mail = SendGrid::Mail.new do |m|
-      m.to = "ricardo.lopez.a@gmail.com"
-      m.from = "params[:email]"
+  #   mail = SendGrid::Mail.new do |m|
+  #     m.to = "ricardo.lopez.a@gmail.com"
+  #     m.from = "params[:email]"
       
-      m.from_name = params[:from_name]
-      m.subject = params[:brand]
+  #     m.from_name = params[:from_name]
+  #     m.subject = params[:brand]
       
-      m.text = params[:message]
-      m.html = "<p style='color: red;'>#{params[:message]}</p>"
-    end
+  #     m.text = params[:message]
+  #     m.html = "<p style='color: red;'>#{params[:message]}</p>"
+  #   end
 
-    # response = client.send(mail)
+  #   # response = client.send(mail)
 
-    if response.code == 200
-      "Thank you. Your email has been sent."
-      redirect_to contact_index_path
+  #   if response.code == 200
+  #     "Thank you. Your email has been sent."
+  #     redirect_to contact_index_path
 
-    else
-      "There has been an error sending your email. Please try again"
-      render :back
-      # redirect_to products_path
-    end
-  end
+  #   else
+  #     "There has been an error sending your email. Please try again"
+  #     render :back
+  #     # redirect_to products_path
+  #   end
+  # end
 
 
 end
