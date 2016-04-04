981a3a987ce77507886aabfca7372ecf98ea3a90
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-04-04 16:07:43 -0400

    pushing for heroku sendgrid test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :contact
   resources :products
+  resources :contacts, only: [:new, :create]
 
   # You can have the root of your site routed with "root"
   root 'products#index'
