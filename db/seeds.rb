662455485ed24e3fdd1157856aecca8ac972eab2
Jesse <jessehwang86@yahoo.com>
2016-04-04 18:05:49 -0400

    intro with new font

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,28 +43,28 @@ end
 
 ######### Maison Margiela
 
-maison_margiela_sneakers = Nokogiri::HTML(open("http://www.neimanmarcus.com/Maison-Margiela/Mens/Shoes/cat48130739/c.cat"))
-margiela_begining_url = "http://www.neimanmarcus.com/"
-margiela_ending_url = maison_margiela_sneakers.css(".product-image-frame a.product-thumbnail-image").map{|link| link["href"]}
+# maison_margiela_sneakers = Nokogiri::HTML(open("http://www.neimanmarcus.com/Maison-Margiela/Mens/Shoes/cat48130739/c.cat"))
+# margiela_begining_url = "http://www.neimanmarcus.com/"
+# margiela_ending_url = maison_margiela_sneakers.css(".product-image-frame a.product-thumbnail-image").map{|link| link["href"]}
 
-margiela_url = []
-for i in 0..margiela_ending_url.length-1 do
-  margiela_url.push(margiela_begining_url + margiela_ending_url[i])
-end
+# margiela_url = []
+# for i in 0..margiela_ending_url.length-1 do
+#   margiela_url.push(margiela_begining_url + margiela_ending_url[i])
+# end
 
-for i in 0..margiela_url.length-1 do
-  maison_margiela_sneaker = Nokogiri::HTML(open(margiela_url[i]))
-  brand = "Maison Margiella"
-  model = maison_margiela_sneaker.at_css(".product-name span").content
-  description = maison_margiela_sneaker.at_css(".product-details-info li").content
-  price = maison_margiela_sneaker.at_css(".lbl_ItemPriceSingleItem").content
-  image_a = maison_margiela_sneaker.at('head meta[property="og:image"]')["content"].chomp("_mb.jpg")
-  image1 = image_a + "_mu.jpg"
-  image_b = maison_margiela_sneaker.at('head meta[property="og:image"]')["content"].chomp("_mb.jpg")
-  image2 = image_b + "_au.jpg"
+# for i in 0..margiela_url.length-1 do
+#   maison_margiela_sneaker = Nokogiri::HTML(open(margiela_url[i]))
+#   brand = "Maison Margiella"
+#   model = maison_margiela_sneaker.at_css(".product-name span").content
+#   description = maison_margiela_sneaker.at_css(".product-details-info li").content
+#   price = maison_margiela_sneaker.at_css(".product-price").content
+#   image_a = maison_margiela_sneaker.at('head meta[property="og:image"]')["content"].chomp("_mb.jpg")
+#   image1 = image_a + "_mu.jpg"
+#   image_b = maison_margiela_sneaker.at('head meta[property="og:image"]')["content"].chomp("_mb.jpg")
+#   image2 = image_b + "_au.jpg"
 
-  Product.create(category: "shoes", category_type: "sneakers", brand: brand, model: model, description: description, price: price, image1: image1, image2: image2)
-end
+#   Product.create(category: "shoes", category_type: "sneakers", brand: brand, model: model, description: description, price: price, image1: image1, image2: image2)
+# end
 
 
 ######### Louis Vitton
