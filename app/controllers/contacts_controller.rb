c697a1d83ddb1cc273f7423060a274a40d191a5e
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-04-04 17:47:59 -0400

    pushing for heroku sendgrid test

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,10 +10,10 @@ class ContactsController < ApplicationController
 
     if @contact.deliver
       flash[:notice] = 'Thank you for your email. We will be in touch.'
+      redirect_to products_path
+    else
+      flash[:error] = 'Cannot send message.'
       render :new
-    # else
-    #   flash[:error] = 'Cannot send message.'
-    #   render :new
 
 
     end
