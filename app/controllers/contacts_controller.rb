7f445a3871ba20095a58190845bedf9920b04831
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-04-04 17:28:21 -0400

    pushing for heroku sendgrid test

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,11 +8,11 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
 
-    if @contact.deliver
-      flash.now[:error] = nil
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
-    end
+    # if @contact.deliver
+    #   flash.now[:error] = nil
+    # else
+    #   flash.now[:error] = 'Cannot send message.'
+    #   render :new
+    # end
   end
 end
